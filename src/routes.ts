30d0ede913c40d6855744f6e05fb8307353bed0e
LeandroDreary <l3androviegas@gmail.com>
2021-06-23 19:48:32 -0300

    v1.2.0

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
+import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
-const createUserControlller = new CreateUserController
+const createUserControlller = new CreateUserController();
+const createTagControlller = new CreateTagController();
 
+router.post("/tags", ensureAdmin, createTagControlller.handle);
 router.post("/users", createUserControlller.handle);
 
 export { router };
